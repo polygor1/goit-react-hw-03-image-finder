9de3be8d67995346b7f351afa98563a4a1e12c5e
Igor <polygor@gmail.com>
2022-01-10 10:04:45 +0200

    url

--- src/components/services/GetListImg.js
+++ src/components/services/GetListImg.js
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 
-const BASE_URL = 'https://pixabay.com/api';
+// const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '24123899-30dafe3a116d297502be19e37';
 const IMG_TYPE = 'photo';
 const IMG_ORIENT = 'horizontal';
 const PER_PAGE = 12;
 
 export default async function GetListImg(query, page) {
-  const url = `${BASE_URL}?image_type=${IMG_TYPE}&orientation=${IMG_ORIENT}&q=${query}&page=${page}&per_page=${PER_PAGE}&key=${API_KEY}`;
+  const url = `https://pixabay.com/api?image_type=${IMG_TYPE}&orientation=${IMG_ORIENT}&q=${query}&page=${page}&per_page=${PER_PAGE}&key=${API_KEY}`;
 
   const response = await fetch(url);
   if (response.ok) {
